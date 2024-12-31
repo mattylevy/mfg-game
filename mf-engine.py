b6b9566ed35e03c9bd304e0f79a9d3e0164055c2
Matt <Matthew.Levy@syngenta.com>
2024-12-31 12:06:37 +0100

    updated

--- mf-engine.py
+++ mf-engine.py
@@ -1,7 +1,6 @@
 import time
-import random
 import redis
-import json
+import os
 
 # Notes #
 # Instead of a "game engine" we call this a Business Engine - a business is just an enitity which is a collection of workflows which create value
@@ -13,32 +12,73 @@ class BusinessEngine:
         self.r = redis.Redis(host='localhost', port=6379, decode_responses=True)
         self.engine_start_time = time.time()
         self.command_queue = 'command_queue'
+        self.frame = 0
+        
+        # game world
+        self.assets = 0
+    
         
         
-    def workflow_input(self):
-        
+    def process_input(self):
+        """ Method to process the command queue """
         # for each new command issued
         while self.r.llen(self.command_queue) > 0:
             command_data = self.r.rpop(self.command_queue)
+            #print(command_data)
             #_, command_json = command_data ** Future build - json payload for actions
             #action = json.loads(command_json) ** Future build - json payload for actions
-            _, action = command_data
+            action = command_data
             
-            # Take action - need action processing function. What
-            # Business logic. What does that action mean? Does it even influence the business state, does it change something. If SO....How does it influence the business state, WHAT IS THE OUTCOME OF THAT ACTION
+            # execute process action logic
+            self.process_action(action)
+            
+    def process_action(self, action):
+        """ 
+        Method to define how actions are processed. 
+        Tells us what each command does and allows us to simulate business logic.
+        """
+        # Take action - need action processing function. What
+        # Business logic. What does that action mean? Does it even influence the business state, 
+        # does it change something. If SO....How does it influence the business state, WHAT IS THE OUTCOME OF THAT ACTION
+        
+        if action =='add_asset':
+            self.assets += 1
+        elif action == 'remove_asset':
+            self.assets -= 1
 
+
+    def update_state(self):
+        """ Method to update business state """
+        self.water_level = 0
+        self.max_height = 5
+        
+
+
+
+    def render(self):
+        """ Render output method """
+        
+        def clear_console():
+            """Clears the console for updated display."""
+            os.system('cls' if os.name == 'nt' else 'clear')
             
+        clear_console()
+        print('Engine running...')
+        print(f'Frame... {self.frame}')
+        print(f'Assets = {self.assets}')
+       
+
         
     def run(self):
         """ Business(game) loop """
         last_time = time.time()
-        target_fps = 0.5  # Slower FPS to make it more readable in this simulation - can we call this clock rate? Or something other to diffrentiate from a game but its the same principle
+        target_fps = 5  # Slower FPS to make it more readable in this simulation - can we call this clock rate? Or something other to diffrentiate from a game but its the same principle
         frame_duration = 1.0 / target_fps
-
 
         while True:
             current_time = time.time()
             delta_time = current_time - last_time
+            
 
             # Ensure non-negative sleep duration
             sleep_duration = frame_duration - delta_time
@@ -47,12 +87,13 @@ class BusinessEngine:
 
             if delta_time >= frame_duration:
 
-                #self.process_input()  # Process any workflow inputs
-                #self.update_business_state(delta_time)  # Update production and resources
-                #self.render()  # Display the current state of the factory as just a text output
+                self.frame += 1
+                self.process_input()  # Process any  inputs
+                self.update_state()  # Update production and resources
+                self.render()  # Display the current state of the factory as just a text output
 
                 last_time = current_time
 
 
-#engine = BusinessEngine()
-#engine.run()
+engine = BusinessEngine()
+engine.run()
---
+++ redisclient.py
@@ -0,0 +1,11 @@
+import redis
+
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+print("\nCommands: [add_asset] Add a new asset, [remove_asset] Removes an asset, [exit] Quit")
+
+while True:
+     # Get user input
+    command = input("Enter a command: ").strip().lower()
+    
+    r.lpush('command_queue',command)
